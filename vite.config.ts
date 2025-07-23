4aaa3e78b2328df3de8803a52ed61c0b90c87d87
Deekshx <151180062+Deekshx@users.noreply.github.com>
2025-07-23 20:32:17 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+/**
+ * 💻 Project developed by Uma Deekshitha
+ * 📅 Created in 2025
+ * 🔗 If you'd like to contribute or use this project, please give proper credit.
+ * 😊 Hope you enjoy working with this project—happy coding!
+ */
+
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
